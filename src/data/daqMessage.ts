fa884ff283ddfa98654fdb0c33aedcfb6d4d6736
Chris Yang <chrisyx511@gmail.com>
2025-11-03 00:00:11 -0500

    Empty Commit

--- src/data/daqMessage.ts
+++
@@ -1,9 +0,0 @@
-type SensorName = string
-
-export interface DAQMessage extends Record<string, unknown> {
-    timestamp: number
-    data: Record<SensorName, number[]>
-    relativeTimestamps: number[]
-    sampleRate: number
-    messageVersion: number
-}
